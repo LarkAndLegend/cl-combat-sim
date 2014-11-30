0ffcfbbfbdb1d25ed1fb42e0b27987a75603f584
Mark E. McDermott <mark.elias.mcdermott@gmail.com>
2014-11-30 01:18:33 -0500

    barebones sender/receiver structures

--- src/bin/cl-combat-sim.rs
+++ src/bin/cl-combat-sim.rs
@@ -1,19 +1,20 @@
 // COSMOLARK COMBAT SIMULATOR
 
-use std::io::net::udp::{UdpSocket};
-use std::io::net::ip::{Ipv4Addr, SocketAddr};
+extern crate cl;
 
 
 fn main() {
 
-    let server_addr = SocketAddr { ip: Ipv4Addr(127,0,0,1), port: 9999};
-    let any_addr = SocketAddr { ip: Ipv4Addr(0,0,0,0), port: 0};
-
-    let mut buf = [0u8, ..64];
-    for s in range(0u,buf.len()) {
+    let mut buf = [0u8, ..cl::message::MAX_BYTES];
+    for s in range(0u,16) {
         buf[s] = s as u8;
     }
 
-    let mut socket = UdpSocket::bind(any_addr).unwrap();
-    socket.send_to(&buf,server_addr).unwrap();    
+    loop {
+        let mut ms = cl::message::MessageSender::new();
+        ms.send_message(buf.slice(0,16));
+
+        std::io::timer::sleep(std::time::Duration::seconds(2));
+    }
+
 }
--- src/bin/cl-cs-server.rs
+++ src/bin/cl-cs-server.rs
@@ -1,21 +1,13 @@
 // COSMOLARK COMBAT SIMULATOR DEDICATED SERVER
 
-use std::io::net::udp::{UdpSocket};
-use std::io::net::ip::{Ipv4Addr, SocketAddr};
-
+extern crate cl;
 
 fn main() {
 
-    let addr = SocketAddr { ip: Ipv4Addr(127,0,0,1), port: 9999 };
+    let mut mr = cl::message::MessageReceiver::new();
 
-    let mut socket = UdpSocket::bind(addr).unwrap();
-
-    let mut buf = [0, ..64];
-
-    socket.recv_from(&mut buf).unwrap();
-
-    for s in range(0u,buf.len()) {
-        println!("{}",buf[s]);
+    loop {
+        mr.process_next_message();
     }
 
 }
