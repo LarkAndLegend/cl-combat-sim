30a8cca99ac4aa73e511e91e933d159ce8b14d76
Mark E. McDermott <mark.elias.mcdermott@gmail.com>
2014-11-29 19:49:18 -0500

    basic udp connections

--- src/bin/cl-combat-sim.rs
+++ src/bin/cl-combat-sim.rs
@@ -1,3 +1,19 @@
+// COSMOLARK COMBAT SIMULATOR
+
+use std::io::net::udp::{UdpSocket};
+use std::io::net::ip::{Ipv4Addr, SocketAddr};
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let server_addr = SocketAddr { ip: Ipv4Addr(127,0,0,1), port: 9999};
+    let any_addr = SocketAddr { ip: Ipv4Addr(0,0,0,0), port: 0};
+
+    let mut buf = [0u8, ..64];
+    for s in range(0u,buf.len()) {
+        buf[s] = s as u8;
+    }
+
+    let mut socket = UdpSocket::bind(any_addr).unwrap();
+    socket.send_to(&buf,server_addr).unwrap();    
 }
--- src/bin/cl-cs-server.rs
+++ src/bin/cl-cs-server.rs
@@ -1,3 +1,21 @@
+// COSMOLARK COMBAT SIMULATOR DEDICATED SERVER
+
+use std::io::net::udp::{UdpSocket};
+use std::io::net::ip::{Ipv4Addr, SocketAddr};
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let addr = SocketAddr { ip: Ipv4Addr(127,0,0,1), port: 9999 };
+
+    let mut socket = UdpSocket::bind(addr).unwrap();
+
+    let mut buf = [0, ..64];
+
+    socket.recv_from(&mut buf).unwrap();
+
+    for s in range(0u,buf.len()) {
+        println!("{}",buf[s]);
+    }
+
 }
